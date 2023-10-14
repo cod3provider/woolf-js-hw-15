c9bcc45f450ed6cc7523616b836ffef76639e872
Ilya Bondarchuk <cod3provider@gmail.com>
2023-10-14 11:40:53 +0300

    third task done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector('.login-form');
+// const email = document.querySelector('input[name="email"]');
+// const password = document.querySelector('input[name="password"]');
 
+
+const handleSubmit = e => {
+    e.preventDefault();
+
+    const email = e.currentTarget.elements.email.value;
+    const password = e.currentTarget.elements.password.value;
+
+    if (email === '' || password === '') {
+        alert('You must fill in all fields');
+    }
+
+    const dataForm = {
+        email,
+        password,
+    }
+
+    console.log(dataForm)
+    form.reset();
+}
+
+form.addEventListener('submit', handleSubmit);

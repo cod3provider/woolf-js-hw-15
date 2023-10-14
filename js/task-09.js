b71e6ddf76fb17322193af404b05961b142a177e
Ilya Bondarchuk <cod3provider@gmail.com>
2023-10-14 11:48:56 +0300

    fourth task done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+const handleClick = () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = `${getRandomHexColor()}`
+}
+
+btnChangeColor.addEventListener('click', handleClick);

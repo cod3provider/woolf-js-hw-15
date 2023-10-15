adf66d5f5e47f5d1af1d4ef7a1c6393f567a7378
Ilya Bondarchuk <cod3provider@gmail.com>
2023-10-15 10:47:41 +0300

    fixed second task

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,7 @@
 const input = document.querySelector('#font-size-control');
 const spanText = document.querySelector('#text');
 
+spanText.style.fontSize = `${input.value}px`;
 const handleChange = e => {
     spanText.style.fontSize = `${e.currentTarget.value}px`;
 }
